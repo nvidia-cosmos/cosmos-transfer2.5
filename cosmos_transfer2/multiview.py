6cb1d3b5dd659507b2df98cc4f12520fe8ef9426
Dan Blick <dblick@nvidia.com>
2026-01-06 00:27:28 +0000

    Bugfix: self.pipe.model.tokenizer

--- cosmos_transfer2/multiview.py
+++ cosmos_transfer2/multiview.py
@@ -185,7 +185,7 @@ class MultiviewInference:
 
         # Calculate number of video frames to load
         assert self.pipe.config.model.config.state_t >= 1
-        chunk_size = self.pipe.tokenizer.get_pixel_num_frames(self.pipe.config.model.config.state_t)
+        chunk_size = self.pipe.model.tokenizer.get_pixel_num_frames(self.pipe.config.model.config.state_t)
         num_video_frames_per_view = chunk_size
         if sample.enable_autoregressive:
             num_video_frames_per_view += (num_video_frames_per_view - sample.chunk_overlap) * (sample.num_chunks - 1)
@@ -278,11 +278,13 @@ class MultiviewInference:
                 log.info(f"------ Generating video ------")
                 if isinstance(num_conditional_frames, list):
                     num_conditional_latent_frames = [
-                        self.pipe.tokenizer.get_latent_num_frames(pixel_frames)
+                        self.pipe.model.tokenizer.get_latent_num_frames(pixel_frames)
                         for pixel_frames in num_conditional_frames
                     ]
                 else:
-                    num_conditional_latent_frames = self.pipe.tokenizer.get_latent_num_frames(num_conditional_frames)
+                    num_conditional_latent_frames = self.pipe.model.tokenizer.get_latent_num_frames(
+                        num_conditional_frames
+                    )
                 batch["num_conditional_frames"] = num_conditional_latent_frames
                 video = self.pipe.generate_from_batch(
                     batch,
